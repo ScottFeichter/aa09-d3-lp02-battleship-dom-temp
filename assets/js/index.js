fbbc32dc62f2868ea3868a568df258aaa8a27ce7
caryn <ca.ryn.ono.w@gmail.com>
2024-04-17 13:32:36 -0700

    createDiv, generateBoard, attempted a grid in css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 import Board from "./board.js";
+// console.log("connected")
 
 let board = new Board(); // creates a new game board
 
@@ -6,4 +7,37 @@ let board = new Board(); // creates a new game board
 // Create the UI of the game using HTML elements based on this grid.
 console.log(board.grid);
 
-// Your code here 
+// Your code here
+
+let body = document.body;
+
+let createDiv = (row, column) => {
+    let div = document.createElement("div");
+
+    let box = `${row}${column}`;
+
+    // div.setAttribute("id", box);
+    div.setAttribute("style", `gridTemplateArea:${box}`)
+
+    body.appendChild(div);
+    return div;
+
+}
+
+let setGridCoordinate = (coordinates, div) => {
+    let rowCol = 
+
+}
+
+let generateBoard = (rowAmount, columnAmount) => {
+    for (let i = 0; i < rowAmount; i++) {
+        for (let j = 0; j < columnAmount; j++) {
+            createDiv(i, j)
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    generateBoard(2, 2);
+
+});
